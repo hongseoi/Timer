3865d07f2854a3da27909f1c6bfc7c87ea15d084
seoi hong <90823683+hongseoi@users.noreply.github.com>
2023-02-09 22:51:26 +0900

    add files

--- timer/README.md
+++ timer/README.md
@@ -2,6 +2,31 @@
 
 A new Flutter project.
 
+## 상태
+
+1. 타이머의 시간
+2. 현재까지 완료한 뽀모도로의 개수
+3. 현재 타이머의 상태
+    * 작업 중 (running)
+    * 정지 (stopped)
+    * 일시정지 (paused)
+    * 휴식 중 (resting)
+
+## 타이머 이벤트 정의하기
+1. 시작하기 버튼 누르기 run: Status.stopped -> Status.running
+2. 작업 타이머 시간이 끝나 휴식 타이머 시작 rest : Status.running -> Status.resting
+3. 일시정지 버튼 누르기 pause: Status.running -> Status.paused
+4. 계속하기 버튼 누르기 resume: Status.paused -> Status.running
+5. 포기하기 버튼 누르기 / 휴식 타이머 시간 끝남 stop : Status.paused or Status.running or Status.resting -> Status.stopped
+
+
+## 타이머 상태 구현에 도움을 주는 문법
+
+enum A {1, 2, 3}
+
+A myVal = A.a;
+if (myVal == A.b) return 'B!';
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- timer/lib/screens/timer_screen.dart
+++ timer/lib/screens/timer_screen.dart
@@ -1,14 +1,120 @@
-import 'dart:html';
+import 'dart:async'; //타이머 클래스 제공하는 패키지
 
 import 'package:flutter/material.dart';
+import 'package:sprintf/sprintf.dart';
 import 'package:timer/style/TextStyle.dart';
 
+//time status 선언
+enum TimerStatus {running, paused, stopped, resting}
+
+//time screen 선언
 class TimerScreen extends StatefulWidget {
   @override
   _TimerScreenState createState() => _TimerScreenState();
 }
 
 class _TimerScreenState extends State<TimerScreen> {
+
+  //state 정의(앱에서 관리할 상태 변수)
+  static const WORK_SECONDS = 25; //*60 min
+  static const REST_SECONDS = 5;  //*60
+
+  late TimerStatus _timerStatus; //타이머의 상태
+  late int _timer; //타이머 시간
+  late int _pomodoroCount; //뽀모도로 개수
+
+
+  @override
+  void initState() { 
+    //override 통해 상태 초기값 설정
+    super.initState();
+    _timerStatus = TimerStatus.stopped; //타이머 상태: 정지상태
+    print(_timerStatus.toString());
+    _timer = WORK_SECONDS; //남은시간 = WORKSECONDS(25초)
+    _pomodoroCount = 0;
+  }
+
+  String secondsToString(int seconds){
+    return sprintf('%2d:%2d', [seconds ~/60, seconds%60]);
+
+  }
+
+  //타이머 이벤트
+  void run(){
+    setState(() {
+      _timerStatus = TimerStatus.running;
+      print("[=>]" + _timerStatus.toString());
+      runTimer();//타이머 실행
+    });
+  }
+
+  void stop(){
+    setState(() {
+      _timer = WORK_SECONDS; //타이머 시간을 초기값으로 변경
+      _timerStatus = TimerStatus.stopped;
+      print("[=>]" + _timerStatus.toString());
+    });
+  }
+
+  void rest(){
+    setState(() {   
+      _timer = REST_SECONDS;
+      _timerStatus = TimerStatus.resting;
+      print("[=>]" + _timerStatus.toString());      
+    });
+  }
+
+  void pause(){
+    setState(){
+      _timerStatus = TimerStatus.paused;
+      print("[=>]" + _timerStatus.toString());
+    }
+  }
+
+  void resume(){
+    run(); //다시시작
+  }
+
+  void runTimer() async{
+    Timer.periodic(Duration(seconds: 1), (Timer t) {
+      switch (_timerStatus){
+        case TimerStatus.paused:
+          t.cancel(); //0초가 되어 타이머 종료
+          break;
+        case TimerStatus.stopped:
+          t.cancel();
+          break;
+        case TimerStatus.running:
+          if (_timer <= 0){
+            print("작업 완료!");
+            rest();
+          }else{
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+        case TimerStatus.resting:
+          if (_timer <= 0){
+            setState(() {
+              _pomodoroCount += 1;
+            });
+            print('오늘 $_pomodoroCount개의 뽀모도로를 달성했습니다.');
+            t.cancel();
+            stop();
+          }else{
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+        default:
+          break;        
+      }
+     });
+  }
+
+
   @override
   Widget build(BuildContext context){
     final List<Widget> _runningButtons = [
@@ -73,8 +179,7 @@ class _TimerScreenState extends State<TimerScreen> {
             children: 1 == 2? 
             const [] : 1== 2? 
             _stoppedButtons:_runningButtons,
-            )
-
+            ),
         ]
         ),
     );
